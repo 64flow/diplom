b2ceea502e6ab7eee1f6d544bc6429fcfa68eaad
64flow <121034198+64flow@users.noreply.github.com>
2024-06-17 15:39:44 +0300

    Update planes.php

--- planes.php
+++ planes.php
@@ -45,7 +45,7 @@ session_start();
 $host = 'aws-0-eu-central-1.pooler.supabase.com';
 $dbname = 'postgres';
 $user = 'postgres.dstcxgfoqwifnystedpz';
-$password = '6412diploM!';
+$password = '###';
 $port = 5432;
 
 try {
